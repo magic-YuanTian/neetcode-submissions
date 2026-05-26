5aeabe130a79d749a30d2966bae60e3408f05970
Yuan Tian <75125334+magic-YuanTian@users.noreply.github.com>
2026-05-26 19:26:31 -0400

    Add: non-cyclical-number - submission-3

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-3.py
@@ -0,0 +1,31 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        
+        seen = {n}
+
+        num = n
+
+        while True:
+            
+            cur = 0
+            
+            while num // 10 > 0:
+                cur += (num % 10) ** 2
+                num = num // 10
+            
+            cur += num ** 2
+
+            if cur == 1:
+                return True
+
+            if cur in seen:
+                return False
+            
+
+
+            seen.add(cur)
+
+            num = cur
+
+                
+
